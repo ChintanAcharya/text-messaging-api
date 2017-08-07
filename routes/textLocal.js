aa9acfb63a8e7389ffea7393151373165ac1c279
Chintan Acharya <acharya.chintan96@gmail.com>
2017-08-07 12:26:46 +0530

    Convert customID to integer

--- routes/textLocal.js
+++ routes/textLocal.js
@@ -2,7 +2,7 @@ module.exports = (db) => (request, response) => {
     const {number, status, customID, datetime} = request.body;
     console.log(request.body);
     db.collection('requests').findOneAndUpdate(
-        {_id: customID, 'numbers.number': number},
+        {_id: +customID, 'numbers.number': number},
         {'$set': {'numbers.$.status': status}}
     )
         .then((result) => {
