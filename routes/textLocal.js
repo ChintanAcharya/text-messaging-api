acad8e5f1c4901aa744cd3c56c918448b5acb55d
Chintan Acharya <acharya.chintan96@gmail.com>
2017-08-07 13:59:41 +0530

    Revert "Insert receipts into database"
    This reverts commit c65a2ac3395ef216c6c8e0431a0b67382b20bfc1.

--- routes/textLocal.js
+++ routes/textLocal.js
@@ -1,14 +1,10 @@
 module.exports = (db) => (request, response) => {
     const {number, status, customID, datetime} = request.body;
     console.log(request.body);
-
-    db.collection('receipts').insertOne(request.body)
-        .then(() =>
-            db.collection('requests').findOneAndUpdate(
-                {_id: +customID, 'numbers.number': number.slice(2)},
-                {'$set': {'numbers.$.status': status}}
-            )
-        )
+    db.collection('requests').findOneAndUpdate(
+        {_id: +customID, 'numbers.number': number.slice(2)},
+        {'$set': {'numbers.$.status': status}}
+    )
         .then((result) => {
             response.json({});
         })
