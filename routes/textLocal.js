c65a2ac3395ef216c6c8e0431a0b67382b20bfc1
Chintan Acharya <acharya.chintan96@gmail.com>
2017-08-07 13:56:23 +0530

    Insert receipts into database

--- routes/textLocal.js
+++ routes/textLocal.js
@@ -1,10 +1,14 @@
 module.exports = (db) => (request, response) => {
     const {number, status, customID, datetime} = request.body;
     console.log(request.body);
-    db.collection('requests').findOneAndUpdate(
-        {_id: +customID, 'numbers.number': number.slice(2)},
-        {'$set': {'numbers.$.status': status}}
-    )
+
+    db.collection('receipts').insertOne(request.body)
+        .then(() =>
+            db.collection('requests').findOneAndUpdate(
+                {_id: +customID, 'numbers.number': number.slice(2)},
+                {'$set': {'numbers.$.status': status}}
+            )
+        )
         .then((result) => {
             response.json({});
         })
